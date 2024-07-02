f8dfcc25091a52576a3e52c426d57f0af239ee0c
Naty03Riveros <121559061+Naty03Riveros@users.noreply.github.com>
2024-07-02 15:22:08 -0400

    modificaciones en entorno virtual

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\natri\OneDrive\Escritorio\Maquetacion_equipo_12\venv
+set VIRTUAL_ENV=C:\Users\natri\OneDrive\Escritorio\Clona_Maquetación_12\Maquetacion_codo_a_codo_12\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
